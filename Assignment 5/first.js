e488b31be272d97d5dd772c524f853edf0b50675
Mohit Kumar <94380889+mohit17g@users.noreply.github.com>
2021-11-24 23:59:37 +0530

    Add files via upload

---
+++ Assignment 5/first.js
@@ -0,0 +1,50 @@
+
+// Write a program to store list of 10 students and print it using for loop?
+
+var arr=["Mohit","Rohit","Suraj","Prakash","Ronak","Sahil","Krrish","sohan","Rajan","Aditya"];
+
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+
+
+
+//JavaScript array method
+
+// var arr=["Mohit","Rohit","Suraj","Prakash","Ronak","Sahil","Krrish","sohan","Rajan","Aditya"];
+
+// console.log(arr.length);
+
+// arr.push("Ram"); // The push() method adds a new element to an array(at the end)
+
+// console.log(arr.length);
+
+// arr.pop("Ram"); // The pop() method removes the last element from an array
+
+// console.log(arr.length);
+
+// arr.shift("Mohit"); //The shift() method removes the first array element and "shifts" all other elements to a lower index.
+
+// console.log(arr.length);
+
+// arr.unshift("Raj"); //The unshift() method adds a new element to an array (at the begining),and "unshifts" older elements.
+
+// console.log(arr.length);
+
+// console.log(arr[1]); //Access an Array item using the index position
+
+// console.log(arr.indexOf("Krrish")); //Find the index of an item in the array
+
+// console.log(arr.toString()); //The javascript method toString() converts an array to a strinf of (comma seperated) array values.
+
+// console.log(arr.join("*")); //The join() method also joins all array elements into a string
+
+// arr[0]="Hritik"; // Changing elements using there index number
+
+// arr[arr.length]="Rock"; // Adding a new element to an array from back 
+
+// delete arr[3]; //using delete leaves undefined holes in the array
+
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
